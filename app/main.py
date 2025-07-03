b1e1bd0ac8de1c9dfa2a093a9282c19076008350
viannik <ivan.nikitski@gmail.com>
2025-07-03 23:52:22 +0300

    Implement context manager for file cleanup

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+from typing import Optional, Type
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> bool:
+        import os
+
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        return False
